d3a42d00dd0e7fa265d01f72b4acd9d8ea36fa07
Brad Bissell <bissell.brad@gmail.com>
2022-12-19 20:34:59 -0500

    cleanup

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@mantine/core';
+
+export default function Footer() {
+  return (
+    <footer>
+      <Text m="1rem" align="center" weight={300}>
+        Designed and implemented by Brad Bissell
+      </Text>
+    </footer>
+  );
+}
--- components/layoutDefault.tsx
+++ components/layoutDefault.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AppBar from './appBar';
+import Footer from './footer';
 
 export default function LayoutDefault({ children }: React.PropsWithChildren) {
   return (
     <>
       <AppBar />
       <main>{children}</main>
-      {/* footer */}
+      <Footer />
     </>
   );
 }
