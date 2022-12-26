582e7854745806aa965c67941bdc91e4573d4db3
Brad Bissell <bissell.brad@gmail.com>
2022-12-25 22:42:34 -0500

    working on infinite scroll on portraits

--- components/Portraits.tsx
+++ components/Portraits.tsx
@@ -1,36 +1,72 @@
 import { Group, Loader } from '@mantine/core';
-import useSWR from 'swr';
+import useSWRInfinite, { SWRInfiniteKeyLoader } from 'swr/infinite';
 import InfiniteScroll from 'react-infinite-scroller';
 import { IllustrationMeta } from '../types/types';
 import Portrait from './Portrait';
+import { useCallback, useEffect, useRef } from 'react';
 
 interface PortraitsProps {
   keyword: string;
 }
 
 export default function Portraits({ keyword }: PortraitsProps) {
+  const PAGE_SIZE = 20;
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data: illustrations, error } = useSWR<[IllustrationMeta]>(
-    `/api/keywords/${keyword}`,
-    fetcher
+  const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData) => {
+    console.log('previous page data: ', previousPageData);
+    if (previousPageData && !previousPageData.length) return null;
+    if (!previousPageData)
+      return `/api/keywords/${keyword}?pageSize=${PAGE_SIZE}`;
+    return `/api/keywords/${keyword}?pageSize=${PAGE_SIZE}&lastItem=${previousPageData.lastItem}`;
+  };
+  const {
+    data: illustrationRequests,
+    error,
+    mutate,
+    size,
+    setSize,
+    isValidating,
+  } = useSWRInfinite<{
+    items: IllustrationMeta[];
+    lastItem: string;
+  }>(getKey, fetcher);
+  console.log('init size: ', size);
+
+  const loader = useRef(null);
+  const handleObserver: IntersectionObserverCallback = useCallback(
+    (entries) => {
+      const target = entries[0];
+      if (target.isIntersecting) {
+        setSize(size + 1);
+        console.log('size change: ', size);
+      }
+    },
+    []
   );
 
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+  }, [handleObserver]);
+
   if (error) return <div>Failed to load</div>;
-  if (!illustrations) return <Loader />;
+  if (!illustrationRequests) return <Loader />;
   return (
     <>
       <Group position="center" spacing="md">
-        {/* <InfiniteScroll
-          pageStart={0}
-          loadMore={loadFunc}
-          hasMore={true || false}
-          loader={<Loader />}
-        > */}
-        {illustrations.map((meta) => (
-          <Portrait key={meta.link} meta={meta} />
-        ))}
-        {/* </InfiniteScroll> */}
+        {illustrationRequests.map((illustrations) =>
+          illustrations.items.map((meta) => (
+            <Portrait key={meta.link} meta={meta} />
+          ))
+        )}
+        <div ref={loader} />
       </Group>
+      <button onClick={() => setSize(size + 1)}>Load More</button>
     </>
   );
 }
--- pages/api/keywords/[keyword].ts
+++ pages/api/keywords/[keyword].ts
@@ -1,4 +1,5 @@
 import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { request } from 'http';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ddbDocClient } from '../../../lib/ddbDocClient';
 
@@ -7,21 +8,26 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
+    const pageSize = 'pageSize' in req.query ? Number(req.query.pageSize) : 20;
+    const startKey = req.query.lastItem ? req.query.lastItem : undefined;
     try {
-      const { Items } = await ddbDocClient.send(
-        new QueryCommand({
-          TableName: process.env.TABLE_NAME,
-          Limit: 20,
-          IndexName: 'Inverted-Index',
-          KeyConditionExpression:
-            'SK = :keyword and begins_with(PK, :sortKeyPrefix)',
-          ExpressionAttributeValues: {
-            ':keyword': `Keyword#${req.query.keyword}`,
-            ':sortKeyPrefix': 'Illustration#',
-          },
-        })
-      );
-      return res.status(200).json(Items);
+      const params = {
+        TableName: process.env.TABLE_NAME,
+        Limit: pageSize,
+        IndexName: 'Inverted-Index',
+        KeyConditionExpression:
+          'SK = :keyword and begins_with(PK, :sortKeyPrefix)',
+        ExpressionAttributeValues: {
+          ':keyword': `Keyword#${req.query.keyword}`,
+          ':sortKeyPrefix': 'Illustration#',
+        },
+        ExclusiveStartKey: startKey,
+      };
+      // @ts-ignore
+      const result = await ddbDocClient.send(new QueryCommand(params));
+      return res
+        .status(200)
+        .json({ items: result.Items, lastItem: result.LastEvaluatedKey });
     } catch (error) {
       console.log(error);
       res.status(400).json(error);
